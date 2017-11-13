dc6363e4d65d71a629b80f4128ee4742b7d3db57
ericfoster <eric_foster73@hotmail.com>
2017-11-12 23:30:28 -0500

    update App to use a component class and set a videos list state

--- src/index.js
+++ src/index.js
@@ -1,25 +1,28 @@
-import React from 'react'; // used to create and manage components
+import React, { Component } from 'react'; // used to create and manage components
 import ReactDOM from 'react-dom'; // used to manipulate the DOM
 import YTSearch from 'youtube-api-search';
-
-// ./ represents current directory
 import SearchBar from './components/search_bar';
-
 const API_KEY = 'AIzaSyBwpPCSpfbb7ihQJe02ci1Ab__fVRow9Z4';
 
-YTSearch({key: API_KEY, term: 'surfboards'}, function(data){
-  console.log(data);
-});
+class App extends Component {
+  constructor(props) {
+    super(props);
 
-// React Term: Downwards Dataflow, means the parent component should manage data
-// this is a functional based component
-// create a new component, This component should produce some html
-const App = () => {
-  return (
-    <div>
-      <SearchBar />
-    </div>
-  );
+    this.state = { videos: [] };
+
+    YTSearch({key: API_KEY, term: 'surfboards'}, videos => {
+      // ES6, if key and property are same variable name, reduces to just the key
+      this.setState({ videos });
+    });
+  }
+  
+  render() {
+    return (
+      <div>
+        <SearchBar />
+      </div>
+    );
+  }
 }
 
 // Take this component's generated HTML and display it 
