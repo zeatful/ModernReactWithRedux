cb1b3c1ca8d9aac7a1ee2bdf285df3d9996aaa62
ericfoster <eric_foster73@hotmail.com>
2017-11-12 23:24:52 -0500

    add initial youtube search call

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -8,13 +8,14 @@ class SearchBar extends Component {
     super(props);
 
     // state is ONLY ever manually updated in a constructor
-    this.state = { term: '' };
+    this.state = { term: '' }; // can use this to set a default value / state
   }
 
   render() {
     // bind the on change event to the onInputChange event handler
     // always update state using setState
     // value set by state makes a controlled component, the value only updates when state changes
+    // a controlled component always us to avoid using jquery to update elements
     return (
       <div>
         <input 
--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
 import React from 'react'; // used to create and manage components
 import ReactDOM from 'react-dom'; // used to manipulate the DOM
+import YTSearch from 'youtube-api-search';
 
 // ./ represents current directory
 import SearchBar from './components/search_bar';
 
-const API_KEY = 'AIzaSyCdV0TbjirYmfYre-nYFgc4Wz-8xSFKqBk';
+const API_KEY = 'AIzaSyBwpPCSpfbb7ihQJe02ci1Ab__fVRow9Z4';
 
+YTSearch({key: API_KEY, term: 'surfboards'}, function(data){
+  console.log(data);
+});
+
+// React Term: Downwards Dataflow, means the parent component should manage data
 // this is a functional based component
 // create a new component, This component should produce some html
 const App = () => {
