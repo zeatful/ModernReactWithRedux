af179614a90b7632ab8f87f2a794a1f2446d0cf0
ericfoster <eric_foster73@hotmail.com>
2017-11-12 22:04:34 -0500

    clean up SearchBar component to use ES6 class syntax

---
+++ src/components/search_bar.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+
+class SearchBar extends Component {
+  render() {
+    return <input />;
+  }
+}
+
+export default SearchBar;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
 import React from 'react'; // used to create and manage components
 import ReactDOM from 'react-dom'; // used to manipulate the DOM
 
+// ./ represents current directory
+import SearchBar from './components/search_bar';
+
 const API_KEY = 'AIzaSyCdV0TbjirYmfYre-nYFgc4Wz-8xSFKqBk';
 
 // create a new component, This component should produce some html
 const App = () => {
-  return <div>Hi!</div>;
+  return (
+  <div>
+    <SearchBar />
+  </div>
+  );
 }
 
 // Take this component's generated HTML and display it 
